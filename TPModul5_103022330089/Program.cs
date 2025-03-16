cddf569f5d6f8aeeb6f87b86bf23451c441247fb
Haafzd <haafizd.alhabib@gmail.com>
2025-03-16 13:48:43 +0700

    Menambahkan class DataGeneric

--- TPModul5_103022330089/Program.cs
+++ TPModul5_103022330089/Program.cs
@@ -6,11 +6,29 @@ public class HaloGeneric {
     }  
 }
 
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
         HaloGeneric halo = new HaloGeneric();
         halo.SapaUser("Haafizd Alhabib Azwir");
+
+        DataGeneric<string> dataNIM = new DataGeneric<string>("103022330089");
+        dataNIM.PrintData();
     }
 }
